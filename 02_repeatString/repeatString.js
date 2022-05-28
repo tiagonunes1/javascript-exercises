279b820624e559dec8b6ced7c8cbb7318ea5c5be
tiagonunes1 <tiagoalexandrenunes1@gmail.com>
2022-05-28 18:51:36 +0100

    resolvido exercicio 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (x, y) {
+  let result = y < 0 ? "ERROR" : "";
+  for (let i = 0; i < y; i++) {
+    result += x;
+  }
+  return result;
 };
 
 // Do not edit below this line
