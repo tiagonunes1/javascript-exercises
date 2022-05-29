a83c2000b8cfe362d310751bc66bf00edc649812
tiagonunes1 <tiagoalexandrenunes1@gmail.com>
2022-05-29 14:10:17 +0100

    resolvido exercicio 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  let temp;
+  if (min > max) {
+    temp = min;
+    min = max;
+    max = temp;
+  }
 
+  const array =
+    typeof min != "number" || typeof max != "number" || min < 0 || max < 0
+      ? "ERROR"
+      : [...Array(max - min + 1).keys()].map((i) => i + min);
+
+  const sum =
+    typeof array === "string"
+      ? "ERROR"
+      : array.reduce((partialSum, a) => partialSum + a, 0);
+
+  return sum;
 };
 
 // Do not edit below this line
