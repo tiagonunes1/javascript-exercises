d1ee6406973331d42b9b4e80544383ad6b2c648c
tiagonunes1 <tiagoalexandrenunes1@gmail.com>
2022-05-29 16:12:22 +0100

    resolvido exercicio 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function (a) {
+  let result = (a - 32) * 0.5556;
+  let formattedResult = Number(result.toFixed(1) == 0.0 ? 0 : result.toFixed(1));
+  return formattedResult;
 };
 
-const ctof = function() {
-
+const ctof = function (a) {
+  let result = a * 1.8 + 32;
+  let formattedResult = Number(result.toFixed(1) == 0.0 ? 0 : result.toFixed(1));
+  return formattedResult;
 };
-
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
