f2c5f636c6662a78352817bdd49eb0516c6683b5
tiagonunes1 <tiagoalexandrenunes1@gmail.com>
2022-05-29 16:00:33 +0100

    resolvido exercicio 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  const result = (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);  
+  return result;
 };
 
 // Do not edit below this line
